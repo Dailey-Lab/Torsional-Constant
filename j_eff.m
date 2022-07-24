e4c285ad93593817e2a384d3f462597bf2a960ad
rentianyi <rentianyi1993@gmail.com>
2022-07-24 17:38:04 -0400

    Update j_eff.m

--- j_eff.m
+++ j_eff.m
@@ -1,16 +1,16 @@
-function [f] =j_eff(rho_uct)
-
-% load('vtr.mat');
+function [tor_const] =j_eff(rho_uct)
 
 currentFolder = pwd;
 loc =  strcat(currentFolder,'\intact'); 
 cd (loc)
 
+% read all the image data for all the images
 Files=dir('*.mat*');
 for k=1:length(Files)
    path1{k}=Files(k).name;
 end
 
+% define some variables
 tor_const=zeros(length(path1),1);
 for ijk=1:length(path1)
 load(path1{ijk});
@@ -18,14 +18,12 @@ test=length(M(:,:,1));
 x = 1:test;
 y = 1:test;
 [X,Y] = meshgrid(x,y);
-
 J=ones(size(M,3),2);
 J(:,1) = [1:size(M,3)]';
-%%
+
+%
 for i=1:size(M,3)
 logical= double(M(:,:,i));
-% logical(logical < rho_uct) = 0;
-% logical=  logical./3500;
 logical(logical < rho_uct) = 0;
 cen=center(i,:);
 Dis_matrix  =  (     ((X-cen(:,2))*0.06e-3).^2  +  ((Y-cen(:,1))*0.06e-3).^2   )  .*(0.06e-3).^2;
@@ -33,7 +31,7 @@ Dis_matrix  =  (     ((X-cen(:,2))*0.06e-3).^2  +  ((Y-cen(:,1))*0.06e-3).^2   )
 J(i,2)=sum(double(logical).*Dis_matrix,'all');
 
 end
-%%
+
 
  J(:,1)=0.06e-3*J(:,1);
  J(:,2)=1./J(:,2);
@@ -42,8 +40,6 @@ end
 
 end
 
-% mdl = fitlm(tor_const,bradan);
-% f = mdl.Rsquared.Ordinary;
-f=tor_const;
+
 cd(currentFolder);
 end
