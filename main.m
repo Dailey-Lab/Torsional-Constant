2cebeb81aa104cb3a570d1a61d131b85e94e42b5
Dailey-Lab <78370986+Dailey-Lab@users.noreply.github.com>
2022-07-25 10:46:41 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,11 +1,24 @@
 clc
 clear all
 
+%% specific the image file folders
+path1={'P00441_MSRU_86_07_RT',...
+    'P00441_MSRU_86_13_TR',...
+};
+
+%% image segementation
+for i=1:length(path1) 
+    [M,center]=imsegmentation(path1{i});
+    filename= strcat(path1{i},'.mat');
+    save(filename,'M','center', '-v7.3')
+end
+
+
 %% define some varialbes
 j_eff=[];
 t=1;
 
-%% density cutoffs i
+%% get j_eff at density cutoffs i
 for i= 675
   j_eff(:,t) = get_jeff(i);   
   t=t+1;
