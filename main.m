24f0b2e87978fb182174d7189391a4dca428b3dd
rentianyi <rentianyi1993@gmail.com>
2022-07-24 22:54:07 -0400

    Update main.m

--- main.m
+++ main.m
@@ -5,12 +5,11 @@ clear all
 j_eff=[];
 t=1;
 
-%% density cutoffs
+%% density cutoffs i
 for i= 675
-  
   j_eff(:,t) = get_jeff(i);   
   t=t+1;
-
 end
 
+%% save the results file
 save('jeff.mat','j_eff')
